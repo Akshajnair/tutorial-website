d9403933a6b06edb2c309aa398645cc11cd5bbdc
Akshaj Nair <34983749+Akshajnair@users.noreply.github.com>
2020-04-04 21:39:38 +0530

    make it better

--- client/src/components/dbcon.js
+++ client/src/components/dbcon.js
@@ -228,24 +228,6 @@ export class dbcon extends Component {
       }
     }
   }
-  imageupload (image, callback) {
-    const fd = new FormData();
-    fd.append('image',image[0])
-    const options = {
-      url: this.state.baseurl + '/images/add',
-      method: 'POST',
-      data: fd,
-      headers: { 'content-type': 'multipart/form-data' }
-    }
-    console.log(image[0])
-    axios(options)
-      .then(res => {
-        console.log(res)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
 }
 
 const db = new dbcon()
